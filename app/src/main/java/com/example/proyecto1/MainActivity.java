5a98abff486ac689cf52bee2b118fc6a5bcfac15
alex <amartinezgonzalez.cf@iesesteveterradas.cat>
2023-09-26 16:46:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/proyecto1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.proyecto1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Holaaa!!!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
